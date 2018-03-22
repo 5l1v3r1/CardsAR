05576120c8d7db7410872998a3784113f63f4d81
Aditya Mitra <amitra.aditya@gmail.com>
2018-03-22 17:48:25 -0400

    Button semi-working

--- app/src/main/java/com/wear/cardsar/MainActivity.java
+++ app/src/main/java/com/wear/cardsar/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,6 +34,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, AddGameActivity.class);
+        startActivity(intent);
     }
 
     @Override
